e6296ba9921e6fff02302aa276225b137c46ad52
pongchul <inchul7511@gmail.com>
2023-08-24 23:14:03 +0900

    Featuring : 회원 도메인 추가

---
+++ backend/src/main/java/com/FBLAB/member/domain/Member.java
@@ -0,0 +1,16 @@
+package com.FBLAB.member.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long memberId;
+}
