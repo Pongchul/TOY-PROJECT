323625ee67b155ab0f169039bc002e8efa6d0dcb
pongchul <inchul7511@gmail.com>
2023-09-01 20:14:39 +0900

    Featuring : 사용자 이름 기능 추가

--- backend/src/main/java/com/FBLAB/backend/member/domain/UserName.java
+++ backend/src/main/java/com/FBLAB/backend/member/domain/UserName.java
@@ -14,6 +14,14 @@ public class UserName {
     private static final int MINIMUM_LENGTH = 1;
     private static final int MAXIMUM_LENGTH = 20;
 
-    @Column(name = "name")
+    @Column(name = "name", length = 20)
     private String value;
+
+    public UserName(String value) {
+        this.value = value;
+    }
+
+    public static UserName from(String value) {
+        return new UserName(value);
+    }
 }
