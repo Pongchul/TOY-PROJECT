5b69601dd14589514932452f602a660dec9002e8
pongchul <inchul7511@gmail.com>
2023-09-05 21:27:53 +0900

    Featuring : Add MemberError Code

--- backend/src/main/java/com/FBLAB/backend/member/exception/MemberErrorCode.java
+++ backend/src/main/java/com/FBLAB/backend/member/exception/MemberErrorCode.java
@@ -7,7 +7,12 @@ public enum MemberErrorCode implements ErrorCode {
 
     USER_ID_SHOULD_NOT_BE_BLANK(400, "MEMBER_01", "사용자의 아이디가 공백입니다."),
     USER_ID_CANNOT_BE_OUT_OF_RANGE(400, "MEMBER_02", "사용자 아이디는 5자 이상 30자 이하입니다."),
-    USER_ID_SHOULD_BE_EMAIL_FORMAT(400, "MEMBER_011", "사용자 아이디가 이메일 형식이 아닙니다."),
+    USER_ID_SHOULD_BE_EMAIL_FORMAT(400, "MEMBER_03", "사용자 아이디가 이메일 형식이 아닙니다."),
+
+    USER_NAME_SHOULD_NOT_BE_BLANK(400,"MEMBER_04","사용자 이름이 공백입니다."),
+    USER_NAME_CANNOT_BE_OUT_OF_RANGE(400,"MEMBER_05","사용자 이름이 2자 이상 20자 이하입니다."),
+
+    PASSWORD_PATTERN_MUST_BE_VALID(400, "MEMBER_06", "올바르지 않는 비밀번호 패턴 입니다.")
     ;
 
     private final int statusCode;
